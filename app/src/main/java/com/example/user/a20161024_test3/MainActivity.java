598bdd96a15cd6156e1db95597318d8edb1a6b9c
lili <liq1862@gmail.com>
2016-10-27 16:10:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/a20161024_test3/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.user.a20161024_test3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    TextView tv,tv2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        tv = (TextView) findViewById(R.id.textView2);
+        tv2 = (TextView) findViewById(R.id.textView3);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //兩種方式
+
+                //TextView itemTV = (TextView) view;
+                //String str = itemTV.getText().toString();
+
+                String[] f = getResources().getStringArray(R.array.fruits);
+                String str = f[position];       //position為被選取元素的索引值
+
+                tv.setText(str);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+    public void Click1(View v)              //按下button後決定輸出內容
+    {
+        int p = spinner.getSelectedItemPosition();
+        String[] f = getResources().getStringArray(R.array.fruits);
+        String str = f[p];
+
+        tv2.setText(str);
+    }
+}
